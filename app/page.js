6c066c1bf34d1e5e2b859dbdd9a4372de712197a
Muhammad Ali Usman <aliusmanijaz143@gmail.com>
2024-10-24 09:25:37 -0700

    chnage the page file

--- app/page.js
+++ app/page.js
@@ -4,10 +4,8 @@ import HeroSectioon from './component/HeroSectioon'
 const Home = () => {
   return (
     <>
-    {/* <AuthorCard2/> */}
     <HeroSectioon/>
-    {/* <AuthorCard/> */}
-      {/* <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi non suscipit, a, sed earum, aspernatur eligendi inventore laborum ea similique eaque sint modi enim doloremque placeat dignissimos officia natus facere!</p> */}
+   
     </>
   )
 }
